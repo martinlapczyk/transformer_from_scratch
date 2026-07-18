b705abf01f672da7507f03473ce69c34d0d83a4b
martinlapczyk <martinlapczyk33@gmail.com>
2026-07-18 12:38:23 -0400

    first batch

--- trans_from_scratch/torch_model/layers.py
+++ trans_from_scratch/torch_model/layers.py
@@ -7,4 +7,15 @@ class FeedForward(nn.Module):
     def forward(self,x):
         return self.net(x)
     
-   
+class PositionalEncoding(nn.Module):
+    def __init__ (self, d_model, max_len=5000):
+        super().__init__()
+        pe=torch.zeros(max_len,d_model)
+        pos=torch.arange(0,max_len).unsqueeze(1).float()
+        div_term=torch.exp(torch.arange(0,d_model,2).float()*-(math.log(10000.0)/d_model))
+        pe[:, 0::2]=torch.sin(pos*div_term)
+        pe[:, 1::2]=torch.cos(pos*div_term)
+        self.register_buffer('pe',pe.unsqueeze(0))
+
+        def forward(self,x):
+            return x+self.pe[:, :x.size(1)]
